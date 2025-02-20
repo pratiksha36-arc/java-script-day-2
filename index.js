8de6fe87d975913e5b531b85c9c690f080f90c7a
pratiksha kumari  [archu] <pratikshapathakn05@gmail.com>
2025-02-20 10:49:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,102 @@
+// console.log ("Hello World");
+// // var num;
+//  num =20;
+// console.log(num);
+// var num;
+
+// num1 = 50;
+// console.log(num1)
+// let num1;
+
+//aritmatic opreators %,+,_,/,*//
+
+console.log ("Hello")
+
+let a=20;
+let b=16;
+let res= a + b;
+
+console.log(res);
+
+let c=20;
+let d=16;
+let res1= c % d;
+
+console.log(res1);
+//moduls//
+let f=20;
+let e=16;
+let res2= e % f;
+
+console.log(res2);
+
+let g="20"
+let h=20;
+let res3= g==h;
+
+console.log(res3);
+
+let i="20";
+let j=20;
+let res4= i===j;
+
+console.log(res4);
+
+//strict equality operator//
+let k=20;
+let l=true
+let res5= k===l;
+
+console.log(res5);
+//not equals to operator
+let m=20;
+let n=true
+let res6= k!=l;
+
+console.log(res6);
+
+let o=20;
+let p="50";
+let res7= o!==p;
+
+console.log(res7);
+
+let q=20;
+let r=20;
+let res8= q!==r;   //one condition is true then its result true 
+
+console.log(res8);
+
+let s=20;
+let t="20";
+let res9= s!==t;
+
+console.log(res9);
+
+let u=20;
+let v=50;
+let res10= u <=v;//one condition is true then it is true
+
+console.log(res10);
+
+let y = 20;
+y=+3;
+console.log(y);
+
+let x=2;
+let z=5;
+console.log(x+z);
+
+let x1=4567;
+let res11= Math.floor(x1/1000);
+
+console.log(res11);
+
+
+
+
+
+
+
+
+
